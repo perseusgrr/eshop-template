de3bc5e6157a9f025e6482a5a2c2fe10aa2e8a1a
Perseus <perseusgrr@gmail.com>
2021-10-22 17:53:21 +0000

    Checkout success page

--- modules/checkout/middlewares/site/checkoutSuccess/validate.js
+++ modules/checkout/middlewares/site/checkoutSuccess/validate.js
@@ -1,12 +1,10 @@
 const { buildSiteUrl } = require("../../../../../lib/routie");
-const { getComponentSource } = require('../../../../../lib/helpers');
 
-module.exports = async (request, response, stack, next) => {
+module.exports = (request, response, stack, next) => {
     if (!request.session.orderId) {
         response.redirect(302, buildSiteUrl("homepage"));
     } else {
         // TODO: Load order
-        request.session.orderId = undefined;
         next();
     }
 };
