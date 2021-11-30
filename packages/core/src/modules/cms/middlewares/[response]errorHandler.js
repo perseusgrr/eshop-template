3565a630200eaf4b4913dede328ebcea0a869909
Perseus <perseusgrr@gmail.com>
2021-11-30 23:35:48 +0000

    Fix winston file path when using pm2

--- packages/core/src/modules/cms/middlewares/[response]errorHandler.js
+++ packages/core/src/modules/cms/middlewares/[response]errorHandler.js
@@ -1,4 +1,6 @@
+const logger = require("../../../lib/log/logger");
+
 module.exports = (err, req, response, stack, next) => {
-    console.log(err);
+    logger.log("error", `Exception in errorHandler middleware`, { message: err.message, stack: err.stack })
     response.status(500).send(err.message);
 };
