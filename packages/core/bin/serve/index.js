e1f5fed19ea30b99360f760f86bafce8e51c7f11
Perseus <perseusgrr@gmail.com>
2022-01-26 15:00:35 +0000

    Fix middleware dependency management

--- packages/core/bin/serve/index.js
+++ packages/core/bin/serve/index.js
@@ -11,14 +11,15 @@ const ora = require('ora');
 const boxen = require('boxen');
 const { app } = require('./app');
 const { addComponents } = require('../../src/lib/componee/addComponents');
-const { getModuleMiddlewares, getAllSortedMiddlewares } = require('../../src/lib/middleware');
+const { getModuleMiddlewares, getAllSortedMiddlewares, getFrontMiddlewares } = require('../../src/lib/middleware');
 const { scanForRoutes } = require('../../src/lib/router/scanForRoutes');
 const { getRoutes, getSiteRoutes, getAdminRoutes } = require('../../src/lib/router/routes');
 const { registerAdminRoute } = require('../../src/lib/router/registerAdminRoute');
 const { registerSiteRoute } = require('../../src/lib/router/registerSiteRoute');
+const { stack } = require('../../src/lib/middleware/stack');
 
 const spinner = ora({
-  text: green('NodeJsCart is starting'),
+  text: green('EverShop is starting'),
   spinner: 'dots12'
 }).start();
 spinner.start();
@@ -153,7 +154,6 @@ app.all('*', (request, response, next) => {
 });
 
 const middlewares = getAllSortedMiddlewares();
-
 middlewares.forEach((m) => {
   if (m.routeId === null) {
     app.use(m.middleware);
@@ -266,7 +266,7 @@ function onError(error) {
 
 function onListening() {
   spinner.succeed(green('Done!!!\n') + boxen(green('Your website is running at "http://localhost:3000"'), {
-    title: 'NodeJsCart', titleAlignment: 'center', padding: 1, margin: 1, borderColor: 'green'
+    title: 'EverShop', titleAlignment: 'center', padding: 1, margin: 1, borderColor: 'green'
   }));
   const addr = server.address();
   const bind = typeof addr === 'string'
