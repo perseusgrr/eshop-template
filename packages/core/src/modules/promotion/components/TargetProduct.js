e75f56e6f49e9faef8a924c64831e9d6800f1d61
Perseus <perseusgrr@gmail.com>
2022-04-22 01:33:27 +0000

    Fix eslint

--- packages/core/src/modules/promotion/components/TargetProduct.js
+++ packages/core/src/modules/promotion/components/TargetProduct.js
@@ -1,8 +1,8 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import React from 'react';
-import Area from "../../../lib/components/Area";
-import { Input } from "../../../lib/components/form/fields/Input";
-import PubSub from "pubsub-js";
+import Area from '../../../lib/components/Area';
+import { Input } from '../../../lib/components/form/fields/Input';
+import PubSub from 'pubsub-js';
 import { FORM_FIELD_UPDATED } from "../../../lib/util/events";
 
 function Products({ targetProducts, maxQty = '' }) {
