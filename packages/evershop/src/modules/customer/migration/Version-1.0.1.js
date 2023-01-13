67b4dc6abe241308723ec2362f16c11e60cae430
Perseus <perseusgrr@gmail.com>
2023-01-13 04:59:18 +0000

    Adding logo

--- packages/evershop/src/modules/customer/migration/Version-1.0.1.js
+++ packages/evershop/src/modules/customer/migration/Version-1.0.1.js
@@ -3,6 +3,11 @@ const { pool } = require('../../../lib/mysql/connection');
 
 // eslint-disable-next-line no-multi-assign
 module.exports = exports = async () => {
-  await execute(pool, `ALTER TABLE \`customer\` ADD \`uuid\` varchar(255) DEFAULT replace(uuid(),'-','') AFTER \`customer_id\``);
-  await execute(pool, `ALTER TABLE \`customer_address\` ADD \`uuid\` varchar(255) DEFAULT replace(uuid(),'-','') AFTER \`customer_address_id\``);
+  await execute(pool, `ALTER TABLE customer MODIFY uuid varchar(36) NOT NULL DEFAULT (replace(uuid(),'-',''))`);
+  await execute(pool, `ALTER TABLE customer ADD UNIQUE KEY \`CUSTOMER_UUID\` (\`uuid\`)`);
+
+  await execute(pool, `ALTER TABLE customer_address MODIFY uuid varchar(36) NOT NULL DEFAULT (replace(uuid(),'-',''))`);
+  await execute(pool, `ALTER TABLE customer_address ADD UNIQUE KEY \`CUSTOMER_ADDRESS_UUID\` (\`uuid\`)`);
+  await execute(pool, `ALTER TABLE customer_address ADD KEY \`FK_CUSTOMER\` (\`customer_id\`)`);
+
 };
