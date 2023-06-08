557cb7896598618d86f7c665340bca5a0f8bc129
Perseus <perseusgrr@gmail.com>
2023-06-08 13:55:05 +0000

    Emailing (SendGrid)

--- packages/evershop/src/modules/customer/migration/Version-1.0.1.js
+++ packages/evershop/src/modules/customer/migration/Version-1.0.1.js
@@ -1,32 +1,67 @@
-const { execute } = require('@evershop/mysql-query-builder');
+const { execute } = require('@evershop/postgres-query-builder');
 
 // eslint-disable-next-line no-multi-assign
 module.exports = exports = async (connection) => {
+  // Create a function to add event to the event table after a customer is created
   await execute(
     connection,
-    "ALTER TABLE customer MODIFY uuid varchar(36) NOT NULL DEFAULT (replace(uuid(),'-',''))"
-  );
-  await execute(
-    connection,
-    'ALTER TABLE customer ADD UNIQUE KEY `CUSTOMER_UUID` (`uuid`)'
-  );
-
-  await execute(
-    connection,
-    "ALTER TABLE customer_address MODIFY uuid varchar(36) NOT NULL DEFAULT (replace(uuid(),'-',''))"
-  );
-  await execute(
-    connection,
-    'ALTER TABLE customer_address ADD UNIQUE KEY `CUSTOMER_ADDRESS_UUID` (`uuid`)'
-  );
-  await execute(
-    connection,
-    'ALTER TABLE customer_address ADD KEY `FK_CUSTOMER` (`customer_id`)'
+    `CREATE OR REPLACE FUNCTION add_customer_created_event() RETURNS TRIGGER AS $$
+    BEGIN
+      INSERT INTO event (name, data)
+      VALUES ('customer_created', row_to_json(NEW));
+      RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;`
   );
 
-  // Add default customer group
+  // Create a trigger to add event to the event table after a customer is created
   await execute(
     connection,
-    "INSERT INTO `customer_group` ( `customer_group_id`, `group_name` ) VALUES (1, 'Default') ON DUPLICATE KEY UPDATE group_name='Default'"
+    `CREATE TRIGGER "ADD_CUSTOMER_CREATED_EVENT_TRIGGER"
+    AFTER INSERT ON customer
+    FOR EACH ROW
+    EXECUTE PROCEDURE add_customer_created_event();`
+  );
+
+  // Create a function to add event to the event table after a customer is updated
+  await execute(
+    connection,
+    `CREATE OR REPLACE FUNCTION add_customer_updated_event() RETURNS TRIGGER AS $$
+    BEGIN
+      INSERT INTO event (name, data)
+      VALUES ('customer_updated', row_to_json(NEW));
+      RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;`
+  );
+
+  // Create a trigger to add event to the event table after a customer is updated
+  await execute(
+    connection,
+    `CREATE TRIGGER "ADD_CUSTOMER_UPDATED_EVENT_TRIGGER"
+    AFTER UPDATE ON customer
+    FOR EACH ROW
+    EXECUTE PROCEDURE add_customer_updated_event();`
+  );
+
+  // Create a function to add event to the event table after a customer is deleted
+  await execute(
+    connection,
+    `CREATE OR REPLACE FUNCTION add_customer_deleted_event() RETURNS TRIGGER AS $$
+    BEGIN
+      INSERT INTO event (name, data)
+      VALUES ('customer_deleted', row_to_json(OLD));
+      RETURN OLD;
+    END;
+    $$ LANGUAGE plpgsql;`
+  );
+
+  // Create a trigger to add event to the event table after a customer is deleted
+  await execute(
+    connection,
+    `CREATE TRIGGER "ADD_CUSTOMER_DELETED_EVENT_TRIGGER"
+    AFTER DELETE ON customer
+    FOR EACH ROW
+    EXECUTE PROCEDURE add_customer_deleted_event();`
   );
 };
