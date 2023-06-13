34785da17adda6eb4f8461dbefb7b523a521678a
Perseus <perseusgrr@gmail.com>
2023-06-13 11:03:07 +0000

    Search function

--- packages/evershop/src/modules/catalog/services/getProductsBaseQuery.js
+++ packages/evershop/src/modules/catalog/services/getProductsBaseQuery.js
@@ -1,7 +1,6 @@
 const { select } = require('@evershop/postgres-query-builder');
-const { pool } = require('@evershop/evershop/src/lib/postgres/connection');
 
-module.exports.getProductsBaseQuery = async (categoryId) => {
+module.exports.getProductsBaseQuery = () => {
   const query = select().from('product');
   query
     .leftJoin('product_description')
@@ -10,17 +9,13 @@ module.exports.getProductsBaseQuery = async (categoryId) => {
       '=',
       'product.product_id'
     );
-  query.where(
-    'product.product_id',
-    'IN',
-    (
-      await select('product_id')
-        .from('product_category')
-        .where('category_id', '=', categoryId)
-        .execute(pool)
-    ).map((row) => row.product_id)
-  );
-  query.andWhere('product.status', '=', 1);
+  query
+    .innerJoin('product_inventory')
+    .on(
+      'product_inventory.product_inventory_product_id',
+      '=',
+      'product.product_id'
+    );
 
   return query;
 };
