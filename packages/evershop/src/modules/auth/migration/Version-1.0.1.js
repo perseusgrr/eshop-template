4a3821bf8ba8f26273fb5b4ca7d4afdc03547e7c
Perseus <perseusgrr@gmail.com>
2023-08-23 06:01:59 +0000

    Improve pagination from admin panel

--- packages/evershop/src/modules/auth/migration/Version-1.0.1.js
+++ packages/evershop/src/modules/auth/migration/Version-1.0.1.js
@@ -1,34 +1,24 @@
-const { execute } = require('@evershop/mysql-query-builder');
+const { execute } = require('@evershop/postgres-query-builder');
 
 // eslint-disable-next-line no-multi-assign
 module.exports = exports = async (connection) => {
+  // Remove user_token_secret table
+  await execute(connection, `DROP TABLE IF EXISTS user_token_secret;`);
+
+  // Create a session table following the `connect-pg-simple` package
   await execute(
     connection,
-    "ALTER TABLE admin_user MODIFY uuid varchar(36) NOT NULL DEFAULT (replace(uuid(),'-',''))"
-  );
-  await execute(
-    connection,
-    'ALTER TABLE admin_user ADD UNIQUE KEY `ADMIN_USER_UUID` (`uuid`)'
+    `CREATE TABLE IF NOT EXISTS session (
+      sid varchar NOT NULL COLLATE "default",
+      sess json NOT NULL,
+      expire timestamp(6) NOT NULL
+    )
+    WITH (OIDS=FALSE);
+    ALTER TABLE session ADD CONSTRAINT "SESSION_PKEY" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;`
   );
 
   await execute(
     connection,
-    'ALTER TABLE user_token_secret ADD COLUMN sid varchar(60) NOT NULL AFTER user_id'
-  );
-  await execute(
-    connection,
-    'ALTER TABLE user_token_secret DROP INDEX `USER_TOKEN_USER_ID`'
-  );
-  await execute(
-    connection,
-    "UPDATE user_token_secret SET sid = replace(uuid(),'-','')"
-  );
-  await execute(
-    connection,
-    'ALTER TABLE user_token_secret ADD UNIQUE KEY `USER_TOKEN_SID_UUID` (`sid`)'
-  );
-  await execute(
-    connection,
-    'ALTER TABLE user_token_secret ADD UNIQUE KEY `USER_TOKEN_SECRET_UUID` (`secret`)'
+    `CREATE INDEX "IDX_SESSION_EXPIRE" ON "session" ("expire");`
   );
 };
